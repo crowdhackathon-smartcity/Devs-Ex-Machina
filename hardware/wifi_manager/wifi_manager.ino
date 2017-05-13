7a3959bd48796a826d3cc5475a920df6c3355a3e
nikost000 <nikost000@gmail.com>
2017-05-13 13:26:34 +0300

    WiFi manager works with some bugs

--- hardware/wifi_manager/wifi_manager.ino
+++ hardware/wifi_manager/wifi_manager.ino
@@ -5,7 +5,8 @@
 #include <WiFiManager.h>  
 
 // BTN pins
-#define PIN_BTN1 1
+const uint8_t BTN_PIN = 3;
+const uint8_t BTN_ID = 122;
 #define BTN_DELAY 2000
 
 // AP Creds
@@ -13,9 +14,11 @@ char ap_name[] = "MagicButton";
 char ap_psk[] = "1241234";
 
 // Host creds
-char host_url[] = "example.com";
-char host_path[] = "/";
-char host_port = 80;
+// char host_url[] = "example.com";
+// char host_port = 80;
+char host_url[] = "83.212.123.145";
+char host_path[] = "/api/btn?id=";
+int host_port = 1880;
 
 
 
@@ -29,23 +32,87 @@ void setup()
 	// Init wifi manager
 	// 
 	WiFiManager wifi_manager;
-Serial.println("Starting2");
-	
-	wifi_manager.setAPStaticIPConfig(IPAddress(10,0,0,1), IPAddress(10,0,0,1), IPAddress(255,255,255,0));\
-Serial.println("Starting3");
-	Serial.println("Connecting...");
+	wifi_manager.setAPStaticIPConfig(IPAddress(10,0,0,1), IPAddress(10,0,0,1), IPAddress(255,255,255,0));
 	wifi_manager.autoConnect(ap_name, ap_psk);
-Serial.println("Starting4");
-	wifi_manager.autoConnect("MagicButton", "12341234");
-Serial.println("Starting5");
 	Serial.println("Connected!");
 
 	// Set IO
-	pinMode(0, INPUT);
+	pinMode(BTN_PIN, INPUT);
+}
+
+/************************************************
+* Send req
+************************************************/
+void send_req(String data, char* host, const char* path, int port)
+{
+	WiFiClient client;
+
+	Serial.printf("Connecting to host %s:%d\n", host, port);
+	if(client.connect(host, port))
+	{
+		Serial.println("Connected to host. Sending req...");
+
+	    Serial.print(data);
+	    client.print(data);
+
+        Serial.println("Response:");
+	    while(client.connected())
+	    {
+	    	if(client.available())
+	    	{
+	        	String line = client.readStringUntil('\n');
+	        	Serial.println(line);
+	    	}
+	    }
+	    client.stop();
+   		Serial.println("\nConnection closed.");
+	}
+	else
+		Serial.println("Failed to connect client.");
+
+	if(WiFi.status() != WL_CONNECTED)
+		Serial.println("Not connected...");
+}
+
+/************************************************
+* Send button req
+************************************************/
+void send_btn_press()
+{
+	String path = host_path + String(BTN_ID);
+
+	String data = String("GET ") + path + " HTTP/1.1\r\n" +
+	                 "Host: " + host_url + "\r\n" +
+	                 "Connection: close\r\n"
+	                 "\r\n";
+
+	send_req(data, host_url, path.c_str(), host_port);
+}
+
+/************************************************
+* Get debounced button status
+************************************************/
+long btn_last_press = 0;
+bool get_btn_status()
+{
+	if(millis() - btn_last_press > BTN_DELAY && digitalRead(BTN_PIN))
+	{
+		btn_last_press = millis();
+		return true;
+	}
+	else
+		return false;
 }
 
 
 void loop()
 {
-
+	// Input button pressed
+	Serial.println(digitalRead(3));
+	//if(get_btn_status())
+	if(digitalRead(BTN_PIN))
+	{
+		Serial.println("btn");
+		send_btn_press();
+	}
 }
